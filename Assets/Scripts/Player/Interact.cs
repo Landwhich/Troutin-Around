4f8cfe006215edb30209601dbdc976cee7d13d28
Landwhich <will.f.richards@gmail.com>
2026-01-30 13:52:27 -0500

    copy + instatiate + delete obj

--- Assets/Scripts/Player/Interact.cs
+++ Assets/Scripts/Player/Interact.cs
@@ -8,7 +8,9 @@ interface IInteractable {
 
 public class Interact : MonoBehaviour
 {
-
+    private GameObject objectBuffer = null;
+    private Vector3 positionBuffer;
+    private Quaternion rotationBuffer;
     public Camera playerCamera;
     public float InteractRange = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,6 +33,31 @@ public class Interact : MonoBehaviour
                 if (hit.collider.gameObject.TryGetComponent(out IInteractable interactObj)) {
                     interactObj.Grab();
                 }
+            }
+        }
+        if (Mouse.current.middleButton.wasPressedThisFrame) {
+            Ray r = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            if (Physics.Raycast(r, out RaycastHit hit, InteractRange)){
+                if (hit.collider.CompareTag("Copyable")) {
+                    objectBuffer = hit.collider.gameObject;
+                    positionBuffer.y = objectBuffer.transform.position.y;
+                    rotationBuffer = objectBuffer.transform.rotation;
+                    Debug.Log("Test Copy");
+                }
+            }
+        }
+        if (Mouse.current.leftButton.wasPressedThisFrame) {
+            Ray r = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            if (Physics.Raycast(r, out RaycastHit hit, InteractRange)){
+                if (hit.collider.gameObject.TryGetComponent(out IInteractable interactObj)) {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+        if (Mouse.current.rightButton.wasPressedThisFrame && objectBuffer != null) {
+            Ray r = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            if (Physics.Raycast(r, out RaycastHit hit, InteractRange)){
+                Instantiate(objectBuffer, new Vector3(hit.point.x, positionBuffer.y, hit.point.z), rotationBuffer);
             }
         }
     }
